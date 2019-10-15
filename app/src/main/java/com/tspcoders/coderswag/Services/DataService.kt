2a87b0cf11cf6a8de91af4e9ecc4cb3c536eb2e1
Joseph Adams <jadams41@nyc.rr.com>
2019-10-15 04:41:19 -0400

    Added view holder

--- app/src/main/java/com/tspcoders/coderswag/Services/DataService.kt
+++ app/src/main/java/com/tspcoders/coderswag/Services/DataService.kt
@@ -20,17 +20,17 @@ object DataService {
     )
 
     val hoodies = listOf(
-        Product("Devslopes Hoodie Gray", "$28", "haoodie01"),
-        Product("Devslopes Hoodie Red", "$32", "haoodie02"),
-        Product("Devslopes Gray Hoodie", "$28", "haoodie03"),
-        Product("Devslopes Black Hoodie", "$32", "haoodie04")
+        Product("Devslopes Hoodie Gray", "$28", "hoodie01"),
+        Product("Devslopes Hoodie Red", "$32", "hoodie02"),
+        Product("Devslopes Gray Hoodie", "$28", "hoodie03"),
+        Product("Devslopes Black Hoodie", "$32", "hoodie04")
     )
 
     val shirts = listOf(
         Product("Devslopes Shirt Black", "$28", "shirt01"),
         Product("Devslopes Badge Light Gray", "$20", "shirt02"),
-        Product("Devslopes Logo Shirt Red", "$22", "haoodie03"),
-        Product("Devslopes Hustle", "$22", "haoodie04"),
-        Product("Kickflip Studios", "$18", "haoodie04")
+        Product("Devslopes Logo Shirt Red", "$22", "shirt03"),
+        Product("Devslopes Hustle", "$22", "shirt04"),
+        Product("Kickflip Studios", "$18", "shirt04")
     )
 }
